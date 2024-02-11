be5877c9f91406d867e6b222227bdbd89611d949
Vicandy23 <dpsd20013@syros.aegean.gr>
2024-02-11 15:39:38 +0200

    button

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -8,9 +8,15 @@ public class ButtonScript : MonoBehaviour
  
     public Text textDisplay;
 
+    void Start()
+    {
+        // Deactivate the deathText when the game starts
+        textDisplay.gameObject.SetActive(false);
+    }
     // This method will be called when the button is clicked
     public void DisplayText()
     {
+        textDisplay.gameObject.SetActive(true);
         textDisplay.text = "Right arrow - right movement" +
             " Left arrow - Left movement" +
             " Space - Jump";
